f89d2b76e0a1cab136fa25ee508b567940f34ca3
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2020-06-10 23:13:10 +0430

    add template

--- myproject/blog/urls.py
+++ myproject/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from blog.views import home
+from blog.views import home, detail
 
 
 app_name = 'blog'
 urlpatterns = [
     path('', home, name='home'),
+    path('article/<slug:slug>', detail, name='detail'),
 ]
--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from .models import Article
 
 def home(request):
 	context = {
-			'title': 'my title',
-			'body': 'my body',
+			'articles': Article.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
+
+def detail(request, slug):
+	context = {
+			'article': Article.objects.get(slug=slug)
+	}
+	return render(request, 'blog/detail.html', context)
