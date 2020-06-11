4f21158360e3cd657a665677a5acf6eec09cf116
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2020-06-11 17:14:58 +0430

    erorr fix

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse, JsonResponse
 from .models import Article
 
 def home(request):
 	context = {
-			'articles': Article.objects.all()
+			'articles': get_list_or_404(Article, status='P')
 	}
 	return render(request, 'blog/home.html', context)
 
 
 def detail(request, slug):
 	context = {
-			'article': Article.objects.get(slug=slug)
+			'article': get_object_or_404(Article, slug=slug, status='P')
 	}
 	return render(request, 'blog/detail.html', context)
