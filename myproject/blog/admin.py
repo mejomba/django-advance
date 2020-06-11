7668f78fa92a0ce2e65e99b5ef423f16b6ead72b
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2020-06-11 21:23:28 +0430

    add category

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+	list_display	= ('title','slug','jupdated','status', 'position')
+	list_filter		= (['status'])
+	search_fields	= ('title', 'slug')
+	prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Category, CategoryAdmin)
 
 
 class ArticleAdmin(admin.ModelAdmin):
-	list_display	= ('title','slug','publish','updated','status')
+	list_display	= ('title','slug','jpublish','jupdated','status', 'category_str')
 	list_filter		= ('publish', 'status')
 	search_fields	= ('title', 'slug', 'description')
 	prepopulated_fields = {'slug': ('title',)}
 	ordering		= ['-status', 'publish']
+
+	def category_str(self, obj):
+		return '، '.join([category.title for category in obj.category.all() if not category.status])
+	category_str.short_description = 'دسته بندی ها'
 
 admin.site.register(Article, ArticleAdmin)
--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,20 +1,55 @@
 from django.db import models
 from django.utils import timezone
+from extension.utils import to_jalali
+
+
+class Category(models.Model):
+	title 		= models.CharField(max_length=200, verbose_name='عنوان دسته')
+	slug		= models.SlugField(max_length=100, unique=True, verbose_name='نام اختصاصی')
+	updated 	= models.DateTimeField(auto_now=True, verbose_name='آخرین بروز زسانی')
+	position 	= models.PositiveIntegerField(unique=True, verbose_name='جایگاه')
+	status 		= models.BooleanField(default=False, verbose_name='عدم نمایش')
+
+
+	class Meta:
+		verbose_name = 'دسته بندی'
+		verbose_name_plural = 'دسته بندی ها'
+		ordering = ['position']
+
+	def __str__(self):
+		return self.title
+
+	def jupdated(self):
+		return to_jalali(self.updated)
+	jupdated.short_description = 'آخرین به روز رسانی'
+
 
 class Article(models.Model):
 	STATUS_CHOICES = (
 			('D', 'پیش نویس'),
 			('P', 'منتشر شده'),
 		)
-	title 		= models.CharField(max_length=200)
-	slug		= models.SlugField(max_length=100, unique=True)
-	description = models.TextField()
-	thumbnail	= models.ImageField(upload_to='images')
-	publish		= models.DateTimeField(default=timezone.now)
-	created		= models.DateTimeField(auto_now_add=True)
-	updated 	= models.DateTimeField(auto_now=True)
-	status 		= models.CharField(choices=STATUS_CHOICES, max_length=1)
+	title 		= models.CharField(max_length=200, verbose_name='عنوان')
+	slug		= models.SlugField(max_length=100, unique=True, verbose_name='نام اختصاصی')
+	description = models.TextField(verbose_name='توضیحات')
+	thumbnail	= models.ImageField(upload_to='images',verbose_name='تصویر')
+	publish		= models.DateTimeField(default=timezone.now, verbose_name='اراعه شده')
+	created		= models.DateTimeField(auto_now_add=True, verbose_name='ایجاد شده')
+	updated 	= models.DateTimeField(auto_now=True, verbose_name='آخرین بروز زسانی')
+	status 		= models.CharField(choices=STATUS_CHOICES, max_length=1, verbose_name='وضعیت')
+	category 	= models.ManyToManyField('Category', verbose_name='دسته بندی')
+	class Meta:
+		verbose_name = 'مقاله'
+		verbose_name_plural = 'مقاله'
 
 
 	def __str__(self):
 		return self.title
+
+	def jpublish(self):
+		return to_jalali(self.publish)
+	jpublish.short_description = 'تاریخ انتشار'
+
+	def jupdated(self):
+		return to_jalali(self.updated)
+	jupdated.short_description = 'آخرین به روز رسانی'
