a6580416ae1c64ef972555b794e4dee7b737ede3
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2020-06-14 03:39:17 +0430

    admin action

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
+from django.contrib import messages
+from django.utils.translation import ngettext
 from .models import Article, Category
+
+def make_published(modeladmin, request, queryset):
+	updated = queryset.update(status='P')
+	modeladmin.message_user(request, ngettext(
+			'%d مقاله با موفقیت منتشر شد.',
+			'%d مقاله با موفقیت منتشر شدند.',
+			updated,
+		) % updated, messages.SUCCESS)
+make_published.short_description = 'انتشار مقاله'
+
+def make_draft(modeladmin, request, queryset):
+	updated = queryset.update(status='D')
+	modeladmin.message_user(request, ngettext(
+			'%d مقاله با موفقیت پیشنویس شد.',
+			'%d مقاله با موفقیت پیشنویس شدند.',
+			updated,
+		) % updated, messages.SUCCESS)
+make_draft.short_description = 'پیشنویس کردن'
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -18,6 +38,7 @@ class ArticleAdmin(admin.ModelAdmin):
 	search_fields	= ('title', 'slug', 'description')
 	prepopulated_fields = {'slug': ('title',)}
 	ordering		= ['-status', 'publish']
+	actions			= [make_published, make_draft]
 
 	def category_str(self, obj):
 		return '، '.join([category.title for category in obj.category.all() if not category.status])
