be1433082c86ee7934a354d78fb468110aeba14b
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2020-07-02 14:47:28 +0430

    superuser can edit all profile form fields

--- myproject/accounts/forms.py
+++ myproject/accounts/forms.py
@@ -4,11 +4,13 @@ from .models import User
 class ProfileForm(forms.ModelForm):
 	"""docstring for ProfileForm"""
 	def __init__(self, *args, **kwargs):
+		user = kwargs.pop('user')
 		super(ProfileForm, self).__init__(*args, **kwargs)
 
-		self.fields['username'].disabled = True
-		self.fields['special_user'].disabled = True
-		self.fields['is_author'].disabled = True
+		if not user.is_superuser:
+			self.fields['username'].disabled = True
+			self.fields['special_user'].disabled = True
+			self.fields['is_author'].disabled = True
 		self.fields['username'].help_text = 'این متن برای راهنمایی است'
 
 	class Meta:
